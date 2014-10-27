c4895209c58556aedfdf63d99eb71c65957f26bc
cristian Angel arrieta sagun <sagun@iota07.aulas.gsyc.es>
2014-10-27 14:58:47 +0100

    terminado

--- server.py
+++ server.py
@@ -28,48 +28,47 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
             print "El cliente nos manda: " + LINE
             clients = LINE.split()[1][4:]
             IP = self.client_address[0]
-            self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
             expires = LINE.split("\r\n")[1][8:]
             metodo = LINE.split("\r\n")[1][:8]
             print "Expires: " + str(expires)
-
             if metodo == "REGISTER":
-                total = time.time() + expires
-                caduca = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(total))
+                self.buscar_clientes(agenda)
+               
+                
                 if expires == 0:
                     del agenda[clients]
                     print "Eliminamos a: " + clients
                 else:
-                    agenda[clients] = IP
+		    total = time.time() + expires
+                    caduca = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(total))
+                    agenda[clients] = [IP, caduca]
+
+            else:
 
                 self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
-                self.register2life(clients, IP, caduca)
-                print "Todos los Clientes: " + agenda
+                print "Clientes: " + agenda
 
-            print "Buscamos clientes: "
-            self.buscar_clientes(agenda)
-
-    def register2life(self, clients, IP, caduca):
-
-        fich = open('registered.txt', 'r+')
-        linea = fich.readlines()
-        if linea ==[]:
-            fich.write('clients' + '\t' + 'IP' + '\t' + 'Expires' + '\n')
+    def register2file(self):
+    #"""
+   # Registramos en el fichero a los clientes
+    #"""        
+        fich = open('registered.txt', 'w')
+        fich.write('clients' + '\t' + 'IP' + '\t' + 'Expires' + '\n')
+        for clave in agenda.clave():
+            ip = agenda[clave][0]
+            expire = str(agenda[clave][1])
+            fich.write(clave + '\t' + IP + '\t' + expire + '\n')
         fich.close()
 
-    def buscar_clientes(self, agenda):
+    def buscar_clientes(self):
 
-        fich = open('registered.txt', 'r')
-        linea = fich.readlines()
-        for cliente in linea:
+        for clave in agenda.claves():
             if len(cliente) != 16:
-                caduca = cliente.split(' ')[1]
+                caduca = agenda[clave][1]
                 hora = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))
-                hora = hora.split(' ')[1]
-                if caduca == hora:
-                    clients = cliente.split(' ')[0]
-                    print clients
+                if caduca <= hora:
                     del agenda[clients]
+                    print "Eliminamos a: " + clave
                     self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
         fich.close()
 
